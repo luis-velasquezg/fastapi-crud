af48f2c0081e8c2af77a165edfa721695fe9acf6
luis-velasquezg <luis.velasquezg@udea.edu.co>
2025-11-01 19:22:08 -0500

    feat: :sparkles: Crea modelo del sistema
    Creación del modelo de usuario

---
+++ models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    name: str = Field(
+        ..., min_length=2, max_length=50, description="Nombre del usuario"
+    )
+    email: EmailStr = Field(..., description="Correo electrónico válido")
+
+
+class UserCreate(UserBase):
+    password: str = Field(
+        ..., min_length=6, description="Contraseña con mínimo 6 caracteres"
+    )
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = Field(None, min_length=2, max_length=50)
+    email: Optional[EmailStr] = None
+    password: Optional[str] = Field(None, min_length=6)
+
+
+class User(UserBase):
+    id: int
